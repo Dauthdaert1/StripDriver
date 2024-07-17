9b27ea280008ac8c69de3cb022c909790feae649
BuildTools <unconfigured@null.spigotmc.org>
2024-07-17 17:30:58 -0500

    Cleaned stuff up and removed build directory

--- components/display_drivers/display_driver.h
+++ components/display_drivers/display_driver.h
@@ -3,6 +3,7 @@
 
 #include "lvgl.h"
 
+//GPIO Pins connected to the LCD display
 #define PIN_NUM_MISO 19
 #define PIN_NUM_MOSI 23
 #define PIN_NUM_CLK  18
@@ -11,9 +12,29 @@
 #define PIN_NUM_RST  33
 #define PIN_NUM_BL   25
 
-void init_display();
-void display_flush_cb(lv_display_t * display, const lv_area_t * area, void * px_map);
-void test1();
+//SPI to use for LCD controller
+#define DISPLAY_SPI     SPI3_HOST
 
+//Display Width and Height
+#define DISPLAY_WIDTH   240
+#define DISPLAY_HEIGHT  240
+
+// Frame buffers 1/5th the size of screen to reduce ram
+// Multiply by 2 for 16 bit color mapping
+#define BUFFER_SIZE 2*DISPLAY_WIDTH*DISPLAY_HEIGHT/5
+
+/** 
+ * Must be called to initialize connection with GC9A01 LCD controller before it can be used.
+ * Initalizes SPI and GPIO for LCD control, as well as registers with LVGL to handle display output.
+ * TODO: Initalize Touch Screen with CST816S touch controller
+*/
+void init_display();
+
+/**
+ * Implementation of function to print the buffer in px_map to the display to be called by LVGL
+ * Following implementation in: https://docs.lvgl.io/master/porting/display.html#flush-cb
+ * TODO: Switch to non-blocking SPI setup so LVGL can work during flush
+ */
+void display_flush_cb(lv_display_t * display, const lv_area_t * area, void * px_map);
 
 #endif
--- components/display_drivers/src/display_driver.c
+++ components/display_drivers/src/display_driver.c
@@ -6,56 +6,39 @@
 #include "esp_log.h"
 #include "lvgl.h"
 
-// Frame buffer 1/5th the size of screen to reduce ram
-#define BUFFER_SIZE 2*240*240/5
-
 spi_device_handle_t spi;
 lv_display_t * display;
 static const char *TAG = "DISPLAY_DRIVER";
 
 static uint8_t frame_buffer[BUFFER_SIZE]; 
 
-//static const uint32_t width = 240;
-//static const uint32_t height = 240;
-
+/**
+ * Used for initalization commands
+ */
 typedef struct {
     uint8_t cmd;
     uint8_t data[16];
-    uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
+    uint8_t databytes; //Bytes of data
 } lcd_init_cmd_t;
 
-static void send_cmd(lcd_init_cmd_t* cmd);
+/***************************************
+ * Static Helper Function Declarations * 
+ ***************************************/
 static void spi_send_cmd(uint8_t cmd);
 static void spi_send_data(void * data, uint32_t size);
 static void disp_spi_send_data(void * data, uint32_t size);
 
-//https://docs.lvgl.io/master/porting/display.html#flush-cb
-void display_flush_cb(lv_display_t * display, const lv_area_t * area, void * px_map){
-    ESP_LOGI(TAG, "FLUSH running, X1: %ld, X2: %ld, Y1: %ld, Y2: %ld", area->x1, area->x2, area->y1, area->y2);
-    uint16_t * buf16 = (uint16_t *)px_map;
-
-    spi_send_cmd(0x2A);
-    uint8_t data_x[4] = {0, area->x1, 0, area->x2};
-    spi_send_data(data_x, 4);
-
-    spi_send_cmd(0x2B);
-    uint8_t data_y[4] = {0, area->y1, 0, area->y2};
-    ESP_LOGI(TAG, "pos %u", ((uint8_t*)&data_y)[2]);
-    spi_send_data(data_y, 4);
-
-    spi_send_cmd(0x2C);
-    ESP_LOGI(TAG, "Size %u", BUFFER_SIZE);
-    spi_send_data(px_map, BUFFER_SIZE);
-
-    lv_display_flush_ready(display);
-}
-
+/** 
+ * Must be called to initialize connection with GC9A01 LCD controller before it can be used.
+ * Initalizes SPI and GPIO for LCD control, as well as registers with LVGL to handle display output.
+ * TODO: Initalize Touch Screen with CST816S touch controller
+*/
 void init_display(){
     esp_err_t ret;
 
     // Configuration for the SPI bus
     spi_bus_config_t buscfg = {
-        .miso_io_num = -1,
+        .miso_io_num = -1,              //Pin is there, but unneeded so ignored
         .mosi_io_num = PIN_NUM_MOSI,
         .sclk_io_num = PIN_NUM_CLK,
         .quadwp_io_num = -1,
@@ -64,12 +47,12 @@ void init_display(){
     };
 
     // Initialize the SPI bus
-    ret = spi_bus_initialize(SPI3_HOST, &buscfg, 1);
+    ret = spi_bus_initialize(DISPLAY_SPI, &buscfg, 1);
     ESP_ERROR_CHECK(ret);
 
     // Configuration for the SPI device on the bus
     spi_device_interface_config_t devcfg = {
-        .clock_speed_hz = 40 * 1000 * 1000, // Clock out at 80 MHz
+        .clock_speed_hz = 80 * 1000 * 1000, // Clock out at 80 MHz
         .mode = 0,                          // SPI mode 0
         .spics_io_num = PIN_NUM_CS,         // CS pin
         .queue_size = 7,                    // Transaction queue size
@@ -77,7 +60,7 @@ void init_display(){
     };
 
     // Attach the SPI device to the SPI bus
-    ret = spi_bus_add_device(SPI3_HOST, &devcfg, &spi);
+    ret = spi_bus_add_device(DISPLAY_SPI, &devcfg, &spi);
     ESP_ERROR_CHECK(ret);
 
     //Configure GPIO
@@ -89,6 +72,7 @@ void init_display(){
     io_conf.pull_up_en = 0;                      // Disable pull-up
     gpio_config(&io_conf);
 
+    //Hit reset on display controller for fresh initialization just in case
     gpio_set_level(PIN_NUM_RST, 0);
     vTaskDelay(100 / portTICK_PERIOD_MS);
     gpio_set_level(PIN_NUM_RST, 1);
@@ -100,7 +84,7 @@ void init_display(){
     //LCD Initalization Commands from: 
     //https://github.com/lvgl/lvgl_esp32_drivers/blob/9fed1cc47b5a45fec6bae08b55d2147d3b50260c/lvgl_tft/GC9A01.c
     lcd_init_cmd_t GC_init_cmds[]={
-////////////////////////////////////////////
+        ////////////////////////////////////////////
 		{0xEF, {0}, 0},
 		{0xEB, {0x14}, 1},
 
@@ -153,23 +137,30 @@ void init_display(){
 		{0x21, {0}, 0},
 		{0x11, {0}, 0x80},	//0x80 delay flag
 		{0x29, {0}, 0x80},	//0x80 delay flag
-        {0x36, {0x68}, 4}, // Set orientation (0x08, 0xC8, 0x68, 0xA8 for "PORTRAIT", "PORTRAIT_INVERTED", "LANDSCAPE", "LANDSCAPE_INVERTED")
+        {0x36, {0x68}, 4},  // Set orientation (0x08, 0xC8, 0x68, 0xA8 for "PORTRAIT", "PORTRAIT_INVERTED", "LANDSCAPE", "LANDSCAPE_INVERTED")
 		{0, {0}, 0xff},		//init end flag
-////////////////////////////////////////////
+        ////////////////////////////////////////////
 	};
 
-    //Send commands
+    //Send init commands
     int curr_cmd = 0;
     while(GC_init_cmds[curr_cmd].databytes!=0xff){
-        send_cmd(&GC_init_cmds[curr_cmd]);
+        spi_send_cmd(GC_init_cmds[curr_cmd].cmd);
+
+        if (GC_init_cmds[curr_cmd].databytes > 0) {
+            spi_send_data(&GC_init_cmds[curr_cmd].data, GC_init_cmds[curr_cmd].databytes);
+        }
+
+        //For commands that need delay
         if (GC_init_cmds[curr_cmd].databytes & 0x80) {
 			vTaskDelay(100 / portTICK_PERIOD_MS);
 		}
         curr_cmd++;
     }
 
+    //Turn on screen
+    //TODO: Make this own function with expanded functionality
     gpio_set_level(PIN_NUM_BL, 1);
-
 
     //Connect to LVGL
     display = lv_display_create(240, 240);
@@ -177,48 +168,55 @@ void init_display(){
     lv_display_set_buffers(display, frame_buffer, NULL, sizeof(frame_buffer), LV_DISPLAY_RENDER_MODE_PARTIAL); //TODO: Second buffer for drawing while DMA SPI works, add later
 }
 
-void test1(){
-    ESP_LOGI(TAG, "Running test program");
+/**
+ * Implementation of function to print the buffer in px_map to the display to be called by LVGL
+ * Following implementation in: https://docs.lvgl.io/master/porting/display.html#flush-cb
+ * TODO: Switch to non-blocking SPI setup so LVGL can work during flush
+ */
+void display_flush_cb(lv_display_t * display, const lv_area_t * area, void * px_map){
+    //Set column space to put data
     spi_send_cmd(0x2A);
-    uint8_t data[4] = {0, 100, 0, 125};
-    spi_send_data(&data, 4);
+    uint8_t data_x[4] = {0, area->x1, 0, area->x2};
+    spi_send_data(data_x, 4);
 
+    //Set row space to put data
     spi_send_cmd(0x2B);
-    spi_send_data(&data, 4);
-    
+    uint8_t data_y[4] = {0, area->y1, 0, area->y2};
+    ESP_LOGI(TAG, "pos %u", ((uint8_t*)&data_y)[2]);
+    spi_send_data(data_y, 4);
+
+    //Flush buffer into data
     spi_send_cmd(0x2C);
-    uint8_t colors[625*2] = {};
-    for(uint32_t i = 0; i < (625*2); i+=2){
-        colors[i] = 0b00000000;
-        colors[i+1] = 0b00011111;
-    }
+    ESP_LOGI(TAG, "Size %u", BUFFER_SIZE);
+    spi_send_data(px_map, BUFFER_SIZE);
 
-    spi_send_data(&colors, 625*2);
-
+    //Let LVGL know flush is complete
+    lv_display_flush_ready(display);
 }
 
-static void send_cmd(lcd_init_cmd_t* cmd) {
-    spi_send_cmd(cmd->cmd);
-
-    if (cmd->databytes > 0) {
-        spi_send_data(&cmd->data, cmd->databytes);
-    }
-}
-
+/**
+ * Sets DC signal to command mode and sends the command over SPI
+ */
 static void spi_send_cmd(uint8_t cmd){
     gpio_set_level(PIN_NUM_DC, 0);
     disp_spi_send_data(&cmd, 1);
 }
 
+/**
+ * Sets DC signal to data mode and sends the data over SPI
+ */
 static void spi_send_data(void * data, uint32_t size){
     gpio_set_level(PIN_NUM_DC, 1);
     disp_spi_send_data(data, size);
 }
 
+/**
+ * Sends specified amount of bytes over SPI to LCD
+ */
 static void disp_spi_send_data(void * data, uint32_t size){
     spi_transaction_t t;
     memset(&t, 0, sizeof(t));       // Zero out the transaction
-    t.length = size * 8;          // Data length in bits
+    t.length = size * 8;            // Data length in bits
     t.tx_buffer = data;             // The data to be sent
     t.user = (void*)1;              // D/C needs to be set to 1
 
